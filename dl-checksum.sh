625ef0588850ca79d54a532f44272246ab09c64d
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-01-01 15:27:47 -0500

    k8s-tew:2.4.10 latest CIs

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -11,11 +11,11 @@ dl_ver()
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
 
     printf "  # %s\n" $url
     printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
 }
 
-dl_ver ${1:-2.4.5}
+dl_ver ${1:-2.4.10}
